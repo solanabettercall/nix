20aea0babb629cedf1e6ba9a7ced99c4a08cbea6
solanabettercall <solanabettercall@gmail.com>
2026-05-17 20:38:57 +0300

    feat(users): add clackgot password hash

--- modules/base/users.nix
+++ modules/base/users.nix
@@ -1,7 +1,9 @@
+{ config, ... }:
 {
   users.users.clackgot = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
+    hashedPasswordFile = config.sops.secrets."users/clackgot/password_hash".path;
     openssh.authorizedKeys.keys = (import ../../ssh-keys.nix).clackgot;
   };
 
